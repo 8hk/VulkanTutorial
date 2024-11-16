2c89f4ec6adf6c2ed07da9e6336a3341c99fbe2a
Hasan Kerem Ocakoglu <39605552+8hk@users.noreply.github.com>
2024-11-16 17:05:05 +0100

    window opening done

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,62 @@
 #include <glm/mat4x4.hpp>
 #include <glm/vec4.hpp>
 #include <iostream>
+#include <vector>
 
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 800, "first window at all", nullptr, nullptr);
+    VkInstance instance;
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        std::cerr << "Failed to create Vulkan instance!" << std::endl;
+        return -1;
+    }
 
-int main(){
+    // Enumerate physical devices
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0)
+    {
+        std::cerr << "No Vulkan physical devices found!" << std::endl;
+        return -1;
+    }
 
-    std::cout<<"done";
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    // Choose the first physical device
+    VkPhysicalDevice physicalDevice = devices[0];
+
+    // Enumerate extensions for the physical device
+    uint32_t extensionCount = 0;
+    vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &extensionCount, extensions.data());
+
+    std::cout << "Available extensions:" << std::endl;
+    for (const auto &extension : extensions)
+    {
+        std::cout << "\t" << extension.extensionName << std::endl;
+    }
+    std::cout << extensionCount << "extension supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vector;
+
+    auto test = matrix * vector;
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    vkDestroyInstance(instance, nullptr);
+    return 0;
 }
